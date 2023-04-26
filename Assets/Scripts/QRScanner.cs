7e1f28bd03ba7a7c18340bf3ee4c6f3387dbacb6
Max Halling <82620224+plebgurt@users.noreply.github.com>
2023-04-26 19:37:56 +0200

    :)

--- Assets/Scripts/QRScanner.cs
+++ Assets/Scripts/QRScanner.cs
@@ -7,10 +7,14 @@ using ZXing;
 public class QRScanner : MonoBehaviour
 { 
     private WebCamTexture webcamTexture;
+    private WebCamTexture webCamTextureBackGround;
+    
     private string QrCode = string.Empty;
     private bool scanningForQR;
-    private RawImage renderer;
+    private RawImage scannerArea;
+    [SerializeField] private RawImage backGround;
 
+    private Texture2D croppedText;
     private void OnEnable()
     {
         WebCamDevice[] devices = WebCamTexture.devices;
@@ -19,11 +23,31 @@ public class QRScanner : MonoBehaviour
             Debug.Log(devices[i].name);
         }
 
+        var rect = GetComponent<Rect>();
+        scannerArea = GetComponent<RawImage>();
+        scannerArea.transform.localScale = new Vector3(-1, 1, -1);
         
-        renderer = GetComponent<RawImage>();
-        renderer.transform.localScale = new Vector3(-1, 1, -1);
+        
+        backGround.transform.localScale = new Vector3(-1, 1, -1);
+        
+        
+        webCamTextureBackGround = new WebCamTexture(WebCamTexture.devices[1].name);
+        backGround.texture = webCamTextureBackGround;
+        
+       
+        
+        
+        /*This works
+        var rect = GetComponent<Rect>();
+        scannerArea = GetComponent<RawImage>();
+        scannerArea.transform.localScale = new Vector3(-1, 1, -1);
         webcamTexture = new WebCamTexture(WebCamTexture.devices[1].name);
-        renderer.texture = webcamTexture;
+        scannerArea.texture = webcamTexture;
+        
+        backGround.transform.localScale = new Vector3(-1, 1, -1);
+        webCamTextureBackGround = webcamTexture;
+        backGround.texture = webCamTextureBackGround;
+         */
     }
     
     private void Update()
@@ -40,14 +64,37 @@ public class QRScanner : MonoBehaviour
         
         scanningForQR = true;
         IBarcodeReader barCodeReader = new BarcodeReader();
-        webcamTexture.Play();
-        var snap = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.ARGB32, false);
+        //webcamTexture.Play();
+       
+        
+        Debug.LogWarning($"Height: {webCamTextureBackGround.height}, Width: {webCamTextureBackGround.width}");
+        
+        webCamTextureBackGround.Play();
+        var camwidth = webCamTextureBackGround.width;
+        var camheight = webCamTextureBackGround.height;
+        
+        
+        Color[] pix = webCamTextureBackGround.GetPixels((int) camwidth / 4, (int) camheight / 4, camwidth / 2,camheight / 2);
+        croppedText = new Texture2D(camwidth / 2, camheight / 2);
+        croppedText.SetPixels(pix);
+        
+        scannerArea.texture = croppedText;
+        
+        var snap = new Texture2D(croppedText.width, croppedText.height, croppedText.format, false);
+        //var snap = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.ARGB32, false);
+        //var snap = croppedText;
         while (string.IsNullOrEmpty(QrCode))
         {
             try
             {
-                snap.SetPixels32(webcamTexture.GetPixels32());
-                var Result = barCodeReader.Decode(snap.GetRawTextureData(), webcamTexture.width, webcamTexture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
+                snap.SetPixels32(croppedText.GetPixels32());
+                
+                if (snap.format != croppedText.format) {
+                    Debug.LogError("Texture format mismatch.");
+                    break;
+                }
+                
+                var Result = barCodeReader.Decode(snap.GetRawTextureData(), croppedText.width, croppedText.height, RGBLuminanceSource.BitmapFormat.ARGB32);
                 
                 if (Result != null)
                 {
@@ -73,5 +120,6 @@ public class QRScanner : MonoBehaviour
     {
         QrCode = string.Empty;
         if(webcamTexture != null) webcamTexture.Stop();
+        if(webCamTextureBackGround != null)webCamTextureBackGround.Stop();
     }
 }
