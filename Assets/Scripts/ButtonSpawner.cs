d5e64817f7fd18e2d09049ad12765e031d3140d9
Max Halling <82620224+plebgurt@users.noreply.github.com>
2023-04-28 17:37:50 +0200

    Unity ui :)
    Screams into the void

--- Assets/Scripts/ButtonSpawner.cs
+++ Assets/Scripts/ButtonSpawner.cs
@@ -7,15 +7,16 @@ public class ButtonSpawner : MonoBehaviour
 {
     [SerializeField] private List<Transform> buttonSpawnPoints;
     [SerializeField] private List<ButtonBase> buttonSO;
-    private List<string> buttonNames = new List<string>();
 
     private void OnEnable()
     {
+        var tempS = "";
         for (int i = 0; i < buttonSpawnPoints.Count; i++)
         {
             if(buttonSpawnPoints[i].transform.childCount > 0) continue;
             buttonSO[i].GenerateButton(buttonSpawnPoints[i]);
-            
+            tempS += $"{buttonSO[i].buttonName}, pos {buttonSpawnPoints[i].position} \n";
         }
+        Debug.Log(tempS);
     }
 }
